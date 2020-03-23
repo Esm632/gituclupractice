ff572ab4f29b90e7a5a2edbb05781f3b2f053b8f
ozgenkacar <ozkcr@icloud.com>
2020-03-23 13:59:37 -0700

    Added reverse method

--- src/test/java/ozgen.java
+++ src/test/java/ozgen.java
@@ -1,2 +1,15 @@
-public class ozgen {
+
+public class ozgen{
+
+    public String reverse(String str) {
+        String result = "";
+        for (int i = str.length(); i >= 0; i--) {
+
+            result += "" + str.charAt(i);
+
+        }
+        return result;
+    }
+
+
 }
