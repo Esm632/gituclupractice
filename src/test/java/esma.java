c5a12370192885e4d02aa07cad4afa70cba55663
Kiymet Koc <kiymetkoc44@gmail.com>
2020-03-23 17:01:24 -0400

    practise

--- src/test/java/esma.java
+++ src/test/java/esma.java
@@ -1,5 +1,14 @@
 public class esma {
     public static void main(String[] args) {
-        System.out.println("Check fetch");
+
     }
+
+    public void reverse(){
+        String result="";
+        for(int i=0; i>=result.length()-1; i--){
+
+        }
+    }
+
+
 }
